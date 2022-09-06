7829f3048b9cc43d3fb925b9bf84b8d1b0f4873e
LiamBaisley <50359625+LiamBaisley@users.noreply.github.com>
2022-09-06 19:08:25 +0200

    Delete StrategyPattern directory

--- StrategyPattern/StrategyPattern/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Behavioural.StrategyPattern;
-
-//Set the strategy to dog
-AnimalContext context = new AnimalContext(new Dog());
-Console.WriteLine(context.DoSound());
-
-//Update the strategy to cat
-context.SetStrategy(new Cat());
-Console.WriteLine(context.DoSound());
