338c799cf07dde8a6eb18cb15bf15f12d21922fb
Liam Baisley <Liam.Baisley@derivco.co.za>
2022-09-06 11:16:09 +0200

    Singleton and factory

---
+++ StrategyPattern/Creational.Singleton/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.Singleton
+{
+    //Class is sealed so that it is not open to inheritance and instantiation 
+    //is restricted to the derived class
+    internal sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if(instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Number of instances " + counter.ToString());
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
