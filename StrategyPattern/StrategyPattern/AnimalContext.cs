a7855cb3a43f15a8bd9f20d20fd2020240532d15
Liam Baisley <Liam.Baisley@derivco.co.za>
2022-09-06 10:10:59 +0200

    Strategy pattern

---
+++ StrategyPattern/StrategyPattern/AnimalContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    internal class AnimalContext
+    {
+        private IAnimal Animal;
+        
+        public AnimalContext(IAnimal animal)
+        {
+            this.Animal = animal;
+        }
+
+        //This method is used to set a new strategy when an instance of AnimalContext already exists
+        public void SetStrategy(IAnimal animal)
+        {
+            this.Animal = animal;
+        }
+
+        public string DoSound()
+        {
+            return Animal.MakeSound();
+        }
+    }
+}
