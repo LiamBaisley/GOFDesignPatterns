743a33e795f045b360212901a0c5b9d9675a0752
Liam Baisley <Liam.Baisley@derivco.co.za>
2022-09-07 12:38:05 +0200

    Builder pattern updates

--- Patterns/Creational.Builder/Program.cs
+++ Patterns/Creational.Builder/Program.cs
@@ -3,10 +3,12 @@
 Animal animal;
 AnimalDirector director = new AnimalDirector();
 
+Console.WriteLine("I am creating a cat for you, here are its details!");
 Cat cat = new Cat();
 animal = director.MakeAnimal(cat);
 animal.DescribePet();
 
+Console.WriteLine("I am creating a dog for you, here are its details!");
 Dog dog = new Dog();
 animal = director.MakeAnimal(dog);
 animal.DescribePet();
