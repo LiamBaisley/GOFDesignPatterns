1584cc3de0012d3fc07029e26d9d44ef071d8065
Liam Baisley <Liam.Baisley@derivco.co.za>
2022-09-08 11:54:01 +0200

    Prototype class

---
+++ Patterns/Creational.Prototype/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.Prototype
+{
+    internal class Cat
+    {
+        //Since there are a couple of kinds of cats this could be cat, lion, leopard etc
+        public string Kind { get; set; }
+        public string Size { get; set; }
+
+        public Cat GetClone()
+        {
+            //This returns a clone of the class, so that edits can be made without modifying other instances. 
+            return (Cat)this.MemberwiseClone();
+        }
+    }
+}
---
+++ Patterns/Creational.Prototype/Program.cs
@@ -0,0 +1,21 @@
+﻿using Creational.Prototype;
+
+Cat Lion = new Cat();
+Lion.Kind = "Lion";
+Lion.Size = "Large";
+
+Console.WriteLine($"A cat was created of type {Lion.Kind} and its size is {Lion.Size}");
+Console.WriteLine("Now we can clone the lion to create a leopard!");
+
+//New instance of the cat class with all the Lion attributes
+Cat Leopard = Lion.GetClone();
+Leopard.Kind = "Leopard";
+
+Console.WriteLine($"a cat was created of type {Leopard.Kind} and its size is {Leopard.Size}");
+Console.WriteLine("Now we can create a House cat from the lion or the leopard object!");
+
+Cat HouseCat = Lion.GetClone();
+HouseCat.Kind = "House cat";
+HouseCat.Size = "Small";
+
+Console.WriteLine($"a cat was created of type {HouseCat.Kind} and its size is {HouseCat.Size}");
